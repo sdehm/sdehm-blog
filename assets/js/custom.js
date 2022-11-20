9775276999c08a29c1f10af65261df0e25430bc8
Shaffer Dehmlow <sdehm@github.com>
2022-11-20 13:01:30 -0700

    Add initial comments custom javascript and css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,41 @@
 import morphdom from "morphdom";
 
-morphdom(document.getElementById("comments"), "<h1>Comments</h1>");
+const socket = new WebSocket("ws://localhost:8080/ws");
 
-console.log("Hello World!");
+// initialize the connection id
+let connectionId = null;
+
+socket.onopen = () => {
+  console.log("Connected");
+};
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+
+  switch (data.type) {
+    case "connected":
+      connectionId = data.connection_id;
+      console.log("Connection ID: " + connectionId);
+      morphdom(document.getElementById("comments"), data.html);
+      document
+        .getElementById("comment-form")
+        .addEventListener("submit", handleCommentSubmit);
+      break;
+    case "morph":
+      morphdom(document.getElementById(data.id), data.html);
+      break;
+  }
+};
+
+function handleCommentSubmit(event) {
+  event.preventDefault();
+  console.log("Submitting comment");
+  const form = event.target;
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const comment = formData.get("comment");
+  console.log("Name: " + name);
+  console.log("Comment: " + comment);
+  // clear the form
+  form.reset();
+};
